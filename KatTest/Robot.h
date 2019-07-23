0b5b665b34cbc8e22f9e70406af2340f80c8e77d
Kathleen Diep <kathleendiep@Kathleens-MacBook-Pro.local>
2019-07-22 21:41:17 -0700

    first commit

---
+++ KatTest/Robot.h
@@ -0,0 +1,17 @@
+//
+//  Robot.h
+//  KatTest
+//
+//  Created by Kathleen Diep on 7/15/19.
+//  Copyright © 2019 Kathleen Diep. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Robot : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KatTest/Robot.m
@@ -0,0 +1,13 @@
+//
+//  Robot.m
+//  KatTest
+//
+//  Created by Kathleen Diep on 7/15/19.
+//  Copyright © 2019 Kathleen Diep. All rights reserved.
+//
+
+#import "Robot.h"
+
+@implementation Robot
+
+@end
